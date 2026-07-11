d1f2e867269d28927f36ef1d930c83d04b9b9574
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-07-11 22:55:41 +0530

    added new program

---
+++ Practice/program025.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int iCnt=0;
+    int iValue=0;
+
+    printf("Enter your frequency: ");
+    scanf("%d",&iValue);
+    
+   for(iCnt = 1; iCnt<=iValue; iCnt++)
+   {
+   printf("Jay ganesh\n");
+   }
+    return 0;
+}

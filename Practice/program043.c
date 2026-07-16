59e2878fe3323d68d6aa48ff954f0b647d526e2c
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-07-17 00:11:04 +0530

    added new program

---
+++ Practice/program043.c
@@ -0,0 +1,26 @@
+//input :8
+//output:  1357 
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt =-iNo; iCnt<=0;iCnt++)  //
+    {
+        printf("%d\t",iCnt);
+    }
+    printf("\n");
+
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter nubmer :");
+    scanf("%d",&iValue);
+    Display(iValue);
+
+
+
+    return 0;
+}

404c1f353f6c0438272ca4cf92be92f1a968dfad
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-07-19 23:41:34 +0530

    added new program

---
+++ Practice/program050.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt=0;
+
+    for(iCnt = 0; iCnt<=5;iCnt++)
+    {
+        printf("Jay gansh\n");
+    }
+
+    return 0;
+}
---
+++ Practice/program052.c
@@ -0,0 +1,19 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt=0;
+    iCnt = 1;
+
+    do
+    {
+        printf("Jay gansh\n");
+
+        iCnt++;
+    }   
+    while( iCnt<=5);
+
+    return 0;
+}

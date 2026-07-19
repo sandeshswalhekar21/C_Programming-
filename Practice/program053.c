aedf1d9c6e20134c38b68c9a174eb3ac1fc168b1
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-07-19 23:42:39 +0530

    added new program

---
+++ Practice/program053.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+int main()
+{
+   int iCnt=0;
+   iCnt=1;
+
+   while (iCnt<=5)
+   {
+    printf("%d",iCnt);
+    iCnt++;
+   }
+   
+   
+
+    return 0;
+}
---
+++ Practice/program054.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main()
+{
+   int iCnt=0;
+   iCnt=5;
+
+   while (iCnt>= 1)
+   {
+    printf("%d",iCnt);
+    iCnt--;
+   }
+    return 0;
+}

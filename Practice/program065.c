5225ca312f02e80200eb4b2afc695ebe32bd3d54
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-07-23 13:58:05 +0530

    added new program

---
+++ Practice/program065.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int Arr[5]={0};
+     int iCnt =0;
+
+    printf("Enter the element : \n");
+    scanf("%d",&Arr[0]);
+    scanf("%d",&Arr[1]);
+    scanf("%d",&Arr[2]);
+    scanf("%d",&Arr[3]);
+    scanf("%d",&Arr[4]);
+
+     printf("Elements of array are:");
+
+    for(iCnt=0;iCnt < 5;iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+    
+
+
+    return 0;
+}

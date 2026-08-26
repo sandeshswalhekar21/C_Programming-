494da58ee16fa0d6700a6e9b5a8cc18b3b915972
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-08-26 23:11:14 +0530

    added new program

---
+++ Practice/program108.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+int main()
+{
+   char *str="Ganesh"; // same as str[]="Ganesh";
+
+   printf("%c\n",*str);
+   str++;
+
+    printf("%c\n",*str);
+   str++;
+
+    printf("%c\n",*str);
+   str++;
+
+    printf("%c\n",*str);
+   str++;
+
+    printf("%c\n",*str);
+   str++;
+
+    printf("%c\n",*str);
+   str++;
+
+    return 0;
+
+}
---
+++ Practice/program109.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+   char *str="Ganesh"; 
+
+   while(*str != '\0')
+   {
+   printf("%c\n",*str);
+   str++;
+   }
+
+   
+
+    return 0;
+
+}

abca90d8eb32fdc665c26624501866f808631401
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-08-26 23:08:38 +0530

    added new program

---
+++ Practice/program102.c
@@ -0,0 +1,13 @@
+//Problems on String
+
+#include<stdio.h>
+
+int main()
+{
+    char str[]= "Jay Ganesh";
+
+    printf("%s\n",str);         //%s formate specifier-. it tell print character until it get '\o'
+
+    return 0;
+
+}
---
+++ Practice/program103.c
@@ -0,0 +1,13 @@
+
+
+#include<stdio.h>
+
+int main()
+{
+    char str[]= {'j','a','y',' ','g','a','n','e','s','h'};     // garbage bcoz not '\0'
+
+    printf("%s\n",str);        
+
+    return 0;
+
+}
